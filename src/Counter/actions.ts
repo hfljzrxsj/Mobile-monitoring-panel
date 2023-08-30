2d777f7eec496710ccfb371c07d297a08e4c56a2
黄俊学 <2506370693@qq.com>
2023-08-30 17:17:16 +0800

    delete

--- src/Counter/actions.ts
+++
@@ -1,9 +0,0 @@
-// actions.ts
-import { type Action } from 'redux';
-const decrement = (): Action<string> => ({
-  'type': 'DECREMENT'
-}),
-  increment = (): Action<string> => ({
-    'type': 'INCREMENT'
-  });
-export { increment, decrement };
